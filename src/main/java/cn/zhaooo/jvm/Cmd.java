b7474e7ee830a58a45b3097fb873ad00ad5dd16f
finezhaooo <635388416@qq.com>
2023-12-15 19:41:11 +0800

    feat: 移除jcommander依赖

--- src/main/java/cn/zhaooo/jvm/Cmd.java
+++ src/main/java/cn/zhaooo/jvm/Cmd.java
@@ -1,56 +1,55 @@
 package cn.zhaooo.jvm;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * cmd
- * C
  */
 public class Cmd {
-    @Parameter(names = "-Xjre", description = "path to jre", order = 4)
-    String jre;
+    String Xjre;
 
-    @Parameter(names = {"-?", "-help"}, description = "print help message", order = 3, help = true)
-    boolean helpFlag = false;
+    boolean help = false;
 
-    @Parameter(names = "-verbose", description = "enable verbose output", order = 5)
-    boolean verboseClassFlag = false;
+    boolean version = false;
+    boolean debug = false;
 
-    @Parameter(names = "-version", description = "print version and exit", order = 2)
-    boolean versionFlag = false;
-
-    @Parameter(names = {"-cp", "-classpath"}, description = "classpath", order = 1)
     String classpath;
-
-    @Parameter(description = "main class and args")
     List<String> mainClassAndArgs;
 
-    // successful parse
-    boolean ok;
-
-    static JCommander jCmd;
-
-    String getMainClass() {
-        return mainClassAndArgs != null && !mainClassAndArgs.isEmpty()
-                ? mainClassAndArgs.get(0)
-                : null;
-    }
-
-    String getAppArgs() {
-        return mainClassAndArgs != null && mainClassAndArgs.size() > 1
-                ? String.join(" ", mainClassAndArgs.subList(1, mainClassAndArgs.size())) : null;
-    }
-
-    static Cmd parse(String[] argv) {
-        Cmd args = new Cmd();
-        jCmd = JCommander.newBuilder().programName("jjvm").addObject(args).build();
-        // JCommander.parse
-        jCmd.parse(argv);
-        args.ok = true;
-        // return Cmd
-        return args;
+    public static Cmd parse(String[] args) {
+        Cmd cmd = new Cmd();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            switch (arg) {
+                case "-h":
+                case "-help":
+                    cmd.help = true;
+                    break;
+                case "-v":
+                case "-version":
+                    cmd.version = true;
+                    break;
+                case "-cp":
+                case "-classpath":
+                    cmd.classpath = args[++i];
+                    break;
+                case "-Xjre":
+                    cmd.Xjre = args[++i];
+                    break;
+                case "-debug":
+                    cmd.debug = true;
+                    break;
+                default:
+                    if (arg.startsWith("-")) {
+                        System.out.printf("Unrecognized option: %s\n", arg);
+                        cmd.help = true;
+                    } else {
+                        cmd.mainClassAndArgs = Arrays.asList(args).subList(i, args.length);
+                        return cmd;
+                    }
+            }
+        }
+        return cmd;
     }
 }
---
+++ src/test/java/helloTest.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: helloTest
+ * @Description:
+ * @Author: zhaooo
+ * @Date: 2023/12/15 19:38
+ */
+public class helloTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
